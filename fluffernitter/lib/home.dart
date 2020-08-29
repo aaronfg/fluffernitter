a0c06edc35b04d23fa4754cada7017b6497cf28d
Aaron Gonzalez <aaron@s57.com>
2020-08-28 21:48:29 -0700

    working. needs an app icon

---
+++ fluffernitter/lib/home.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  StreamSubscription _sub;
+
+  String tLink = 'You haven\'t tapped any Twitter links yet.';
+  String errMsg = '';
+
+  @override
+  void initState() {
+    _initUniLinks();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _sub.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  'fluffernitter',
+                  style: TextStyle(
+                      color: Color.fromRGBO(255, 108, 96, 1.0),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 60),
+                  child: Text(
+                    'You have to tap a Twitter.com link for this app to do anything.',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                if (errMsg.isNotEmpty)
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text('Error: $errMsg'),
+                  ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text('Last link:\n$tLink',
+                    textAlign: TextAlign.center, style: Theme.of(context).textTheme.caption),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<Null> _initUniLinks() async {
+    _sub = getUriLinksStream().listen((Uri uri) {
+      if (!mounted) return;
+      setState(() {
+        errMsg = '';
+      });
+      _launchURL(_makeNitterUri(uri));
+    }, onError: (err) {
+      // Handle exception by warning the user their action did not succeed
+      setState(() {
+        errMsg = 'Failed to open';
+      });
+    });
+    try {
+      Uri initialUri = await getInitialUri();
+      if (initialUri != null) {
+        print('-------------');
+        print('REDIRECT TO: ${initialUri.toString()}');
+        setState(() {
+          errMsg = '';
+        });
+        _launchURL(_makeNitterUri(initialUri));
+      } else {
+        print('null!!!!!');
+      }
+    } on PlatformException {
+      setState(() {
+        errMsg = 'Something was busted getting the url. Sorry.';
+      });
+    }
+  }
+
+  _makeNitterUri(Uri tUri) {
+    final Uri nUri = Uri(
+      scheme: 'https',
+      host: 'nitter.net',
+      path: tUri.path,
+    );
+    return nUri;
+  }
+
+  _launchURL(Uri yuri) async {
+    if (await canLaunch(yuri.toString())) {
+      setState(() {
+        tLink = yuri.toString();
+        errMsg = '';
+      });
+      await launch(yuri.toString());
+    } else {
+      setState(() {
+        errMsg = 'Could not launch ${yuri.toString()}';
+      });
+    }
+  }
+}
